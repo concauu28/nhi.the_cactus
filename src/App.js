0264e20858f9a19082226899e92de8446d95d34a
Viet Tien <vdang074@uottawa.ca>
2023-03-09 14:42:19 -0500

    add logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Animation from "./component/Animation";
 import Word from "./component/Word";
+import Image from "./component/icon.png";
+import Header from "./Header";
 function App() {
   return (
     <div className="App">
+      {/* <div className="header">
+        <img src={Image} alt="cactus icon" />
+        <Word className="word"></Word>
+      </div> */}
+      <Header className="header"></Header>
       <Animation className="animation"></Animation>
-      <Word className="word"></Word>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+import Word from "./component/Word";
+import Image from "./component/icon.png";
+function Header() {
+  return (
+    <div className="header">
+      <img src={Image} alt="cactus icon" />
+      <Word className="word"></Word>
+    </div>
+  );
+}
+
+export default Header;
